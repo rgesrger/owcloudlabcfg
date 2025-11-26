8b576e8cd9ee5258a0efee20a0464aaa387b8f72
Daniel Xiong <daniel@daniels-mbp-64.lan>
2025-11-26 10:46:12 -0500

    a

--- profile.py
+++ profile.py
@@ -3,23 +3,21 @@ import geni.rspec.pg as rspec
 
 request = rspec.Request()
 
-# User can still select OS image if desired
+# Optional OS image (Ubuntu 22.04)
 portal.context.defineParameter(
     "os_image", "OS Image", portal.ParameterType.IMAGE,
     "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD",
-    longDescription="The OS image to install on the node."
+    longDescription="The operating system image to install on the node."
 )
 
 params = portal.context.bindParameters()
 
 master_node = request.RawPC("k8s-master-node")
 
-# master_node.component_id = params.node_type   <-- removed
-
-# ✔ Only set the OS image
+# Only set OS image
 master_node.disk_image = params.os_image
 
-# Startup script
+# Run startup script
 master_node.addService(rspec.Execute(
     shell="bash",
     command="/bin/bash /local/repository/startup.sh > /local/repository/startup.log 2>&1"
